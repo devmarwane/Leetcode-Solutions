a385c605c0b7ae469b57fb907cdd163dfd93b114
Marwane Bidamou <devr.marwane@gmail.com>
2024-09-15 21:52:09 -0500

    Time: 114 ms (75.00%) | Memory: 50.8 MB (89.41%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int[] ans = new int[nums.Length*2];
+        for(int i=0;i<nums.Length;i++) {
+            ans[i] = nums[i];
+            ans[i+nums.Length] = nums[i];
+        }
+        return ans;
+    }
+}
