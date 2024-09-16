d80ce031389dfe6da256a975f868d3e76bec8093
Marwane Bidamou <devr.marwane@gmail.com>
2024-09-15 21:52:24 -0500

    Time: 70 ms (53.27%) | Memory: 41.9 MB (67.53%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.Length;i++){
+            if (nums[i] == target || nums[i]>target)
+                return i;
+        }
+        return nums.Length;
+    }
+}
