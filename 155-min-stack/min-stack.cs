2d10eb4337a900f523547b70bd4d21912b6985fc
Marwane Bidamou <devr.marwane@gmail.com>
2024-09-21 23:01:23 -0500

    Time: 115 ms (46.05%) | Memory: 55.3 MB (65.50%) - LeetSync

---
+++ 155-min-stack/min-stack.cs
@@ -0,0 +1,37 @@
+public class MinStack {
+
+    Stack<int> stack;
+    Stack<int> minStack;
+    
+    public MinStack() {
+        stack = new Stack<int>();
+        minStack = new Stack<int>();
+    }
+    
+    public void Push(int val) {
+        stack.Push(val);
+        minStack.Push(Math.Min(val,minStack.Count != 0?minStack.Peek():val));
+    }
+    
+    public void Pop() {
+        stack.Pop();
+        minStack.Pop();
+    }
+    
+    public int Top() {
+        return stack.Peek();
+    }
+    
+    public int GetMin() {
+        return minStack.Peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.Push(val);
+ * obj.Pop();
+ * int param_3 = obj.Top();
+ * int param_4 = obj.GetMin();
+ */
