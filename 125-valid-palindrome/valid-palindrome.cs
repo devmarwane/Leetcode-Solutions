26d1d1f7ef36a53c605bd3019f3c23a020d92192
Marwane Bidamou <devr.marwane@gmail.com>
2024-09-28 22:45:34 -0500

    Time: 47 ms (98.63%) | Memory: 43.4 MB (83.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cs
+++ 125-valid-palindrome/valid-palindrome.cs
@@ -2,23 +2,27 @@ using System.Text.RegularExpressions;
 
 public class Solution {
     public bool IsPalindrome(string s) {
-        // remove all non-alphanumeric characters
-        Regex rgx = new Regex("[^a-zA-Z0-9]");
-        s = rgx.Replace(s, "");
-        //Remove empty spaces & convert uppercase letters into lowercase
-        s=s.ToLower().Replace(" ","");
-        if (string.IsNullOrEmpty(s))
+       
+  
+        if (string.IsNullOrWhiteSpace(s))
             return true;
 
         int l=0;
         int r=s.Length-1;
 
-        while(l<(s.Length/2)+1){
-            if (s[l]!=s[r])
-                return false;
-
-            l++;
-            r--;
+        while(l<r){
+            if (!char.IsLetterOrDigit(s[l]))
+                l++;
+            else if (!char.IsLetterOrDigit(s[r]))
+                r--;
+            else
+            {
+                if (char.ToLower(s[l]) != char.ToLower(s[r]))
+                    return false;
+                
+                l++;
+                r--;
+            }
         }
 
         return true;
