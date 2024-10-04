741b3a8589331fcf122816f15eca997d8012c7ce
Marwane Bidamou <devr.marwane@gmail.com>
2024-10-04 13:58:09 -0500

    Time: 23 ms (45.23%) | Memory: 27.5 MB (5.32%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int ClimbStairs(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+
+        Dictionary<int, int> map  = new Dictionary<int, int>();
+
+        return ClimbStairsHelper(n, map);
+    }
+
+    private int ClimbStairsHelper(int n, Dictionary<int, int> map)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+
+        if (map.ContainsKey(n))
+            return map[n];
+
+
+        int val= ClimbStairsHelper(n - 1, map) + ClimbStairsHelper(n - 2, map);
+        map.Add(n, val);
+        return val;
+    }
+
+}
