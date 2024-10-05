1237a97736db128f5c35a1e080ebdccac170bb39
Marwane Bidamou <devr.marwane@gmail.com>
2024-10-05 11:07:53 -0500

    Time: 19 ms (84.16%) | Memory: 26.5 MB (86.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cs
+++ 1013-fibonacci-number/fibonacci-number.cs
@@ -1,34 +1,18 @@
 public class Solution {
-    public int Fib(int n) {
-        Dictionary<int,int> memo = new Dictionary<int,int>();
-        return FibHelper(n, memo);
-    }
 
-    private int FibHelper(int n, Dictionary<int,int> memo)
-    {
-        if (n == 0)
-            return 0;
-        
-        if (n == 1)
-            return 1;
-        
-        int n1, n2;
-        if (!memo.ContainsKey(n-1)){
-            n1 = FibHelper(n-1, memo);
-            memo.Add(n-1, n1);
-        }
-        else{
-            n1 = memo[n-1];
+     public int Fib(int n) {
+        if (n<2)
+            return n;
+
+        int[] fNumbers = new int[n+1];
+
+        fNumbers[0] = 1;
+        fNumbers[1] = 1;
+
+        for(int i=2;i<n;i++){
+            fNumbers[i]=fNumbers[i-1]+fNumbers[i-2];
         }
 
-        if (!memo.ContainsKey(n-2)){
-            n2 = FibHelper(n-2, memo);
-            memo.Add(n-2, n2);
-        }
-        else{
-            n2 = memo[n-2];
-        }
-
-        return n1 + n2;
+        return fNumbers[n-1];
     }
 }
