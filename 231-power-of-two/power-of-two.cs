cc37a7d885767f1f49a862f2fccd795151a29d76
Marwane Bidamou <devr.marwane@gmail.com>
2024-10-04 20:58:30 -0500

    Time: 30 ms (59.59%) | Memory: 28.7 MB (57.64%) - LeetSync

--- 231-power-of-two/power-of-two.cs
+++ 231-power-of-two/power-of-two.cs
@@ -1,20 +1,14 @@
 public class Solution {
     public bool IsPowerOfTwo(int n) {
-       if (n == 0)
-            return false;
-        
-        if (n == 1 || n ==  2)
+       if (n == 1 || n ==  2)
             return true;
 
+       if (n == 0 || n%2!=0)
+            return false;
+        
+        
 
-        int x=1;
-        int sum = 2;
-        while(x<n){
-            sum*=2;
-            if (sum == n)
-                return true;
-            x++;
-        }
-        return false;
+        return IsPowerOfTwo(n/2);
+        
     }
 }
