8023bcf2e2a14845b70aa8f28727d9d483661304
Marwane Bidamou <devr.marwane@gmail.com>
2024-10-07 22:37:07 -0500

    Time: 73 ms (6.61%) | Memory: 42.7 MB (7.98%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cs
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        // Create a dummy node to simplify list creation
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;  // Pointer to construct the new list
+
+        while (list1 != null || list2 != null) {
+            if (list1 == null) {
+                // Add the rest of list2
+                current.next = new ListNode(list2.val);
+                list2 = list2.next;
+            } 
+            else if (list2 == null) {
+                // Add the rest of list1
+                current.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } 
+            else {
+                // Compare values and add the smaller one to the merged list
+                if (list2.val <= list1.val) {
+                    current.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                } 
+                else {
+                    current.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }
+            }
+            // Move the pointer forward to the newly added node
+            current = current.next;
+        }
+
+        // Return the merged list, skipping the dummy node
+        return dummy.next;
+    }
+}
