1913593e686ef7bb7a80d7c69c9c6ec6f54dc3d0
Marwane Bidamou <devr.marwane@gmail.com>
2024-10-11 20:37:54 -0500

    Time: 122 ms (61.44%) | Memory: 47.8 MB (40.80%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -1,20 +1,14 @@
 public class Solution {
-         public int[] TwoSum(int[] nums, int target)
- {
-     Dictionary<int, int> hash = new Dictionary<int, int>();
-     for (int i = 0; i < nums.Length; i++)
-     {
-         var r = target - nums[i];
-         
-         if (hash.ContainsKey(r))
-         {
-             return new int[] { hash[r], i };
-         }
-         else if (!hash.ContainsKey(nums[i]))
-         {
-             hash.Add(nums[i], i);
-         }
-     }
-     return new int[] { -1, -1 };
- }
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> indices = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            var diff = target - nums[i];
+            if (indices.ContainsKey(diff)) {
+                return new int[] {indices[diff], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return null;
+    }
 }
