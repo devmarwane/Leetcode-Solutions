cf7b6791ac851548f37929c428779f9a45bb6b09
Marwane Bidamou <devr.marwane@gmail.com>
2024-10-11 21:21:54 -0500

    Time: 128 ms (67.60%) | Memory: 50.6 MB (40.95%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cs
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cs
@@ -1,5 +1,8 @@
 public class Solution {
     public int[] TopKFrequent(int[] nums, int k) {
+
+        int[] topK = new int[k];
+
         Dictionary<int,int> numsFrequency = new Dictionary<int,int>();
         for(int i=0;i<nums.Length;i++)
         {
@@ -8,6 +11,20 @@ public class Solution {
             }
         }
 
-        return numsFrequency.OrderByDescending(x=>x.Value).Take(k).Select(x=>x.Key).ToArray();
+        //Use a priority queue (min-heap) to track the top k frequent elements
+        var pq = new PriorityQueue<int, int>();
+        // Iterate over the frequency map and add each element to the priority queue
+        foreach (var key in numsFrequency.Keys)
+        {
+            pq.Enqueue(key, numsFrequency[key]);// Add element and its frequency as priority
+            if (pq.Count > k) pq.Dequeue();  // Keep only top k elements in the priority queue
+        }
+
+        //Extract elements from the priority queue and store them in the result array
+        int index = k;
+        while (pq.Count > 0) {
+            topK[--index] = pq.Dequeue();//// Dequeue the elements in reverse order into topK
+        }
+        return topK;
     }
 }
