edf8ac5d9441b2e21a78d76f5d12166852c19bd2
Marwane Bidamou <devr.marwane@gmail.com>
2024-10-13 22:08:43 -0500

    Time: 141 ms (67.18%) | Memory: 57.6 MB (30.41%) - LeetSync

--- 283-move-zeroes/move-zeroes.cs
+++ 283-move-zeroes/move-zeroes.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
-
-        int filled=0;
-        for(int i=0;i<nums.Length;i++){
-            if(nums[i] != 0){
-                if(i!=filled){
-                    nums[filled] = nums[i];
-                    nums[i]=0;
-                }
-                filled++;
+        int lastNonZeroIndex = 0;
+        
+        for (int i = 0; i < nums.Length; i++) {
+            // When we encounter a non-zero element, we swap it with the element at the last known non-zero index
+            if (nums[i] != 0) {
+                int temp = nums[lastNonZeroIndex];
+                nums[lastNonZeroIndex] = nums[i];
+                nums[i] = temp;
+                lastNonZeroIndex++;
             }
-        }          
+        }
     }
 }
